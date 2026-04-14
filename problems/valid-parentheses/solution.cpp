cfb5707e850a5c2fbbd4a93cfb12a55f9322cd8e
iijung <987kmj4@naver.com>
2026-04-14 18:40:19 +0900

    solve: valid-parentheses

--- problems/valid-parentheses/solution.cpp
+++ problems/valid-parentheses/solution.cpp
@@ -3,5 +3,21 @@
 class Solution {
 public:
     bool isValid(string s) {
+        stack<char> stack;
+        for (const char& c: s) {
+            if (c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else if (stack.empty())
+                return false;
+            else if (c == ')' && (stack.top() != '('))
+                return false;
+            else if (c == '}' && (stack.top() != '{'))
+                return false;
+            else if (c == ']' && (stack.top() != '['))
+                return false;
+            else
+                stack.pop();
+        }
+        return stack.empty();
     }
 };
