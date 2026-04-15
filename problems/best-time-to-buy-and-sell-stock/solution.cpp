1c910de435ace01f359d16025eba2bf1d93c881a
iijung <987kmj4@naver.com>
2026-04-15 23:41:39 +0900

    solve: best-time-to-buy-and-sell-stock

--- problems/best-time-to-buy-and-sell-stock/solution.cpp
+++ problems/best-time-to-buy-and-sell-stock/solution.cpp
@@ -3,5 +3,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int buy_price = prices[0];
+        for (int day = 1; day < prices.size(); ++day) {
+            const int new_profit = prices[day] - buy_price;
+            if (new_profit < 0)
+                buy_price = prices[day];
+            else if (new_profit > profit)
+                profit = new_profit;
+        }
+        return profit;
     }
 };
