5fab48e966e23eefa0950819d03bc140d518facd
iijung <987kmj4@naver.com>
2026-04-16 00:08:19 +0900

    solve: valid-palindrome

--- problems/valid-palindrome/solution.cpp
+++ problems/valid-palindrome/solution.cpp
@@ -3,5 +3,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        string palindrome = "";
+        for (const char& c : s) {
+            if (isalnum(c)) {
+                palindrome += tolower(c);
+            }
+        }
+        const int len = palindrome.size();
+        const int mid = len / 2;
+        for (int i = 0; i < mid; ++i) {
+            if (palindrome[i] != palindrome[len - i - 1])
+                return false;
+        }
+        return true;
     }
 };
