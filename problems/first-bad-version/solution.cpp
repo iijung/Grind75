afd42d96824b4bab6b2478fb783904a66ba7b976
iijung <987kmj4@naver.com>
2026-04-16 18:15:53 +0900

    solve: first-bad-version

--- problems/first-bad-version/solution.cpp
+++ problems/first-bad-version/solution.cpp
@@ -6,5 +6,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
+        unsigned int left = 1;
+        unsigned int right = n;
+
+        while (left <= right) {
+            const unsigned int mid = left + (right - left) / 2;
+            if (isBadVersion(mid))
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return left;
     }
 };
