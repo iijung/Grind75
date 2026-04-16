29c740799ee059081cdaf33ee3cd2c4d2a698923
iijung <987kmj4@naver.com>
2026-04-16 10:53:45 +0900

    solve: invert-binary-tree

--- problems/invert-binary-tree/solution.cpp
+++ problems/invert-binary-tree/solution.cpp
@@ -14,5 +14,13 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        root->left = invertTree(right);
+        root->right = invertTree(left);
+        return root;
     }
 };
