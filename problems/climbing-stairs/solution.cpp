0538140eac3adec0050a706d016f4a19f2ded76c
iijung <987kmj4@naver.com>
2026-04-16 19:03:53 +0900

    solve: climbing-stairs

--- problems/climbing-stairs/solution.cpp
+++ problems/climbing-stairs/solution.cpp
@@ -3,5 +3,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        if (n < 4)
+            return n;
+        if (cache[n] != 0)
+            return cache[n];
+        cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return cache[n];
     }
+private:
+    int cache[46] = {0, 1, 2, 3};
 };
