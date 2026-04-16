7d93d76adf70e2ca05ec13436aed84678060b2eb
iijung <987kmj4@naver.com>
2026-04-16 16:54:37 +0900

    solve: balanced-binary-tree

--- problems/balanced-binary-tree/solution.cpp
+++ problems/balanced-binary-tree/solution.cpp
@@ -14,5 +14,20 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
+        return dfs(root) != -1;
+    }
+
+    int dfs(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        const int left = dfs(root->left);
+        if (left == -1)
+            return -1;
+        const int right = dfs(root->right);
+        if (right == -1)
+            return -1;
+        if (abs(left - right) > 1)
+            return -1;
+        return max(left, right) + 1;
     }
 };
