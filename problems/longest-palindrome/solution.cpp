f296ac78ab59adb6a9a94e8526234e5df0e14e4a
iijung <987kmj4@naver.com>
2026-04-16 19:46:53 +0900

    solve: longest-palindrome

--- problems/longest-palindrome/solution.cpp
+++ problems/longest-palindrome/solution.cpp
@@ -3,5 +3,23 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
+        int alpabet[2][26] = {0};
+        for (int i = 0; i < s.length(); ++i) {
+            if (isupper(s[i])) {
+                alpabet[0][s[i] - 'A']++;
+            } else {
+                alpabet[1][s[i] - 'a']++;
+            }
+        }
+        int length = 0;
+        bool can_add = false;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                length += alpabet[i][j] / 2 * 2;
+                if (alpabet[i][j] % 2)
+                    can_add = true;
+            }
+        }
+        return length + (can_add? 1 : 0);
     }
 };
