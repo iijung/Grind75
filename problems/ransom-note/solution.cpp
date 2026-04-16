2c1e9de13262bde474190c65bda7fb7be240f2d5
iijung <987kmj4@naver.com>
2026-04-16 18:29:30 +0900

    solve: ransom-note

--- problems/ransom-note/solution.cpp
+++ problems/ransom-note/solution.cpp
@@ -3,5 +3,15 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        int alphabet[26] = {0};
+        for (int i = 0; i < magazine.length(); ++i)
+            alphabet[magazine[i] - 'a']++;
+        for (int i = 0; i < ransomNote.length(); ++i) {
+            const char idx = ransomNote[i] - 'a';
+            if (alphabet[idx] == 0)
+                return false;
+            alphabet[idx]--;
+        }
+        return true;
     }
 };
