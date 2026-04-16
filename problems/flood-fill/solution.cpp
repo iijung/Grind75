c037f8447ed8bc034c47c7cb00daebc9ea70254a
iijung <987kmj4@naver.com>
2026-04-16 12:10:35 +0900

    refactor: use direction array for extensibility

--- problems/flood-fill/solution.cpp
+++ problems/flood-fill/solution.cpp
@@ -5,19 +5,22 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         if (image[sr][sc] == color)
             return image;
-        const int original_color = image[sr][sc];
 
-        const int max_sr = image.size() - 1;
-        const int max_sc = image[sr].size() - 1;
+        const int original_color = image[sr][sc];
         image[sr][sc] = color;
-        if (sr > 0 && original_color == image[sr - 1][sc])
-            floodFill(image, sr - 1, sc, color);
-        if (sr < max_sr && original_color == image[sr + 1][sc])
-            floodFill(image, sr + 1, sc, color);
-        if (sc > 0 && original_color == image[sr][sc - 1])
-            floodFill(image, sr, sc - 1, color);
-        if (sc < max_sc && original_color == image[sr][sc + 1])
-            floodFill(image, sr, sc + 1, color);
+
+        const vector<pair<const int, const int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int i = 0; i < directions.size(); ++i) {
+            const int new_sr = sr + directions[i].first;
+            const int new_sc = sc + directions[i].second;
+            if (new_sr < 0 || new_sr >= image.size())
+                 continue;
+            if (new_sc < 0 || new_sc >= image[new_sr].size())
+                 continue;
+            if (image[new_sr][new_sc] != original_color)
+                 continue;
+            floodFill(image, new_sr, new_sc, color);
+        }
         return image;
     }
 };
