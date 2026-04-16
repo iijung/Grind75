a8ed4c89b8fb7ac744d7818a97496f5ec66ee40f
iijung <987kmj4@naver.com>
2026-04-16 17:59:38 +0900

    init: first-bad-version

---
+++ problems/first-bad-version/solution.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/first-bad-version/
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+    }
+};
