355a46516e25fa02b93c8f8f2b21fb476b6caacb
iijung <987kmj4@naver.com>
2026-04-18 21:26:43 +0900

    solve: majority-element

--- problems/majority-element/solution.cpp
+++ problems/majority-element/solution.cpp
@@ -3,5 +3,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for (const int& num: nums) {
+            count[num]++;
+        }
+        const int mid = nums.size() / 2;
+        for (const auto& [num, cnt]: count) {
+            if (cnt > mid)
+                return num;
+        }
+        return -1;
     }
 };
