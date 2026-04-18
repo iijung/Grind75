13238a718aee3aff3363b4a17999650efd62e159
iijung <987kmj4@naver.com>
2026-04-18 22:47:11 +0900

    solve: add-binary

--- problems/add-binary/solution.cpp
+++ problems/add-binary/solution.cpp
@@ -3,5 +3,20 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
+        if (a == "0" && b == "0")
+            return "0";
+        int i = 0;
+        int carry = 0;
+        string result = "";
+        while (i < a.length() || i < b.length() || carry) {
+             if (i < a.length())
+                carry += a[a.length() - 1 - i] - '0';
+            if (i < b.length())
+                carry += b[b.length() - 1 - i] - '0';
+            result = (carry & 1? "1" : "0") + result;
+            carry /= 2;
+            ++i;
+        }
+        return result;
     }
 };
