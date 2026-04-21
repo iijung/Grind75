5eb13506a36ae787dd80c192baa75c80c7f5e453
iijung <987kmj4@naver.com>
2026-04-21 21:37:25 +0900

    test: for add-binary

---
+++ problems/add-binary/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#include "solution.cpp"
+
+void test(string a, string b, string expected) {
+    string output = Solution().addBinary(a, b);
+    if (output == expected)
+        return ;
+    cout << "# addBinary(" << a << ", " << b << ")";
+    cout << " expect: " << expected << endl;
+    cout << " output: " << output << endl;
+}
+
+int main() {
+    test("0", "0", "0");
+    test("1", "0", "1");
+    test("1", "1", "10");
+    test("11", "1", "100");
+    test("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+            "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011",
+            "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000");
+    return 0;
+}
+
+
