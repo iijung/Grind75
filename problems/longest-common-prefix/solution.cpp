466f93cf0543b8c9506f0fa7f1859211fb5447c0
iijung <987kmj4@naver.com>
2026-04-22 04:40:01 +0900

    solve: longest-common-prefix

--- problems/longest-common-prefix/solution.cpp
+++ problems/longest-common-prefix/solution.cpp
@@ -3,5 +3,16 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        int prefix_len = 0;
+        while (!strs.empty() && prefix_len < strs[0].length()) {
+            const char& c = strs[0][prefix_len];
+            for (int i = 1; i < strs.size(); ++i) {
+                if (c != strs[i][prefix_len])
+                    goto FINISH;
+            }
+            prefix_len++;
+        }
+FINISH:
+        return strs[0].substr(0, prefix_len);
     }
 };
