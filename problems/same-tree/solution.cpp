ed0aaa60dbce62f41afa18750fed91c5909c213d
iijung <987kmj4@naver.com>
2026-04-22 02:29:00 +0900

    solve: same-tree

--- problems/same-tree/solution.cpp
+++ problems/same-tree/solution.cpp
@@ -14,5 +14,19 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        return dfs(p, q);
+    }
+private:
+    bool dfs(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+        if (p == nullptr || q == nullptr) {
+            return false;
+        }
+        if (p->val != q->val) {
+            return false;
+        }
+        return dfs(p->left, q->left) && dfs(p->right, q->right);
     }
 };
