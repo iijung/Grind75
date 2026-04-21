63b4cc93e2ac236444e80c1d5fb469e93201a2c2
iijung <987kmj4@naver.com>
2026-04-22 02:07:37 +0900

    solve: counting-bits

--- problems/counting-bits/solution.cpp
+++ problems/counting-bits/solution.cpp
@@ -3,5 +3,18 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        vector<int> result(n + 1);
+        for (int i = 0; i <= n; ++i)
+            result[i] = popcount(i);
+        return result;
+    }
+private:
+    int popcount(int n) {
+        n = (n & 0x55555555) + (n >> 1 & 0x55555555);
+        n = (n & 0x33333333) + (n >> 2 & 0x33333333);
+        n = (n & 0x0f0f0f0f) + (n >> 4 & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + (n >> 8 & 0x00ff00ff);
+        n = (n & 0x0000ffff) + (n >> 16 & 0x0000ffff);
+        return n;
     }
 };
