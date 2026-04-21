96d33950533209bd189f7ffb4f8c6d2b42049e0b
iijung <987kmj4@naver.com>
2026-04-22 04:48:57 +0900

    solve: single-number

--- problems/single-number/solution.cpp
+++ problems/single-number/solution.cpp
@@ -3,5 +3,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (const int& num: nums)
+            result ^= num;
+        return result;
     }
 };
