845fda66e1fc006d698ed2b1dd256de833a7b5c4
iijung <987kmj4@naver.com>
2026-04-21 21:46:14 +0900

    solve: contains-duplicate

--- problems/contains-duplicate/solution.cpp
+++ problems/contains-duplicate/solution.cpp
@@ -3,5 +3,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, bool> is_exist;
+        for (const int& num: nums) {
+            if (is_exist[num])
+                return true;
+            is_exist[num] = true;
+        }
+        return false;
     }
 };
