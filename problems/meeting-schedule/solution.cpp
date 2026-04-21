3c722a52607c628e54d307b5c770d2797cf58a2a
iijung <987kmj4@naver.com>
2026-04-21 22:20:23 +0900

    init: meeting-schedule

---
+++ problems/meeting-schedule/solution.cpp
@@ -0,0 +1,20 @@
+// https://neetcode.io/problems/meeting-schedule
+
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+    }
+};
+
