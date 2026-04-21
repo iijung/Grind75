3a68d3acca2a0a78c3d4ce12223443f6eef9cbdb
iijung <987kmj4@naver.com>
2026-04-22 05:17:53 +0900

    solve: palindrome-linked-list

--- problems/palindrome-linked-list/solution.cpp
+++ problems/palindrome-linked-list/solution.cpp
@@ -13,5 +13,24 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        if (head == nullptr)
+            return true;
+        stack<int> s;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next) {
+            s.push(slow->val);
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if (fast)
+            slow = slow->next;
+        while (slow) {
+            if (slow->val != s.top())
+                return false;
+            s.pop();
+            slow = slow->next;
+        }
+        return true;
     }
 };
