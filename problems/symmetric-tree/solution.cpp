95c79968769f5834e1b2aca2b1821b38e9cc790c
iijung <987kmj4@naver.com>
2026-04-22 22:48:17 +0900

    solve: use dfs in symmetric-tree

--- problems/symmetric-tree/solution.cpp
+++ problems/symmetric-tree/solution.cpp
@@ -14,5 +14,22 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        return dfs(root->left, root->right);
+    }
+private:
+    bool dfs(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr)
+            return true;
+        if (left == nullptr || right == nullptr)
+            return false;
+        if (left->val != right->val)
+            return false;
+        if (dfs(left->left, right->right) == false)
+            return false;
+        if (dfs(left->right, right->left) == false)
+            return false;
+        return true;
     }
 };
