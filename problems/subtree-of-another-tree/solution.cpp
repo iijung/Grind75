752e1189ea7823588efecb145f739209187564cc
iijung <987kmj4@naver.com>
2026-04-23 00:45:30 +0900

    solve: subtree-of-another-tree

--- problems/subtree-of-another-tree/solution.cpp
+++ problems/subtree-of-another-tree/solution.cpp
@@ -14,5 +14,31 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (subRoot == nullptr)
+            return true;
+        if (root == nullptr)
+            return false;
+        if (root->val == subRoot->val && isIdentical(root, subRoot))
+            return true;
+        if (isSubtree(root->left, subRoot))
+            return true;
+        if (isSubtree(root->right, subRoot))
+            return true;
+        return false;
+
+    }
+private:
+    bool isIdentical(TreeNode* s, TreeNode* t) {
+        if (s == nullptr && t == nullptr)
+            return true;
+        if (s == nullptr || t == nullptr)
+            return false;
+        if (s->val != t->val)
+            return false;
+        if (!isIdentical(s->left, t->left))
+            return false;
+        if (!isIdentical(s->right, t->right))
+            return false;
+         return true;
     }
 };
