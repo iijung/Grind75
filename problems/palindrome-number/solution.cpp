ab753defe91113e582fe86c33770b1fb85ed7a90
iijung <987kmj4@naver.com>
2026-04-22 23:25:00 +0900

    solve: palindrome-number

--- problems/palindrome-number/solution.cpp
+++ problems/palindrome-number/solution.cpp
@@ -3,5 +3,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int digit = 1;
+        while (digit <= x / 10)
+            digit *= 10;
+        while (x) {
+            if (x / digit != x % 10)
+                return false;
+            x %= digit;
+            x /= 10;
+            digit /= 100;
+        }
+        return true;
     }
 };
