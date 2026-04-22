c10419c21cca8bc3774250bc9e1e49803b445613
iijung <987kmj4@naver.com>
2026-04-22 23:52:51 +0900

    solve: convert-sorted-array-to-binary-search-tree

--- problems/convert-sorted-array-to-binary-search-tree/solution.cpp
+++ problems/convert-sorted-array-to-binary-search-tree/solution.cpp
@@ -14,5 +14,16 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBST(nums, 0, nums.size());
+    }
+private:
+    TreeNode* sortedArrayToBST(vector<int>& nums, int start, int end) {
+        if (start >= end)
+            return nullptr;
+        const int mid = (start + end) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = sortedArrayToBST(nums, start, mid);
+        root->right = sortedArrayToBST(nums, mid + 1, end);
+        return root;
     }
 };
