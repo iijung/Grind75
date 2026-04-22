f1e8863690b62e16b8f3e27d20e7459198f50889
iijung <987kmj4@naver.com>
2026-04-23 02:06:38 +0900

    solve: maximum-subarray

--- problems/maximum-subarray/solution.cpp
+++ problems/maximum-subarray/solution.cpp
@@ -3,5 +3,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (const int& num: nums) {
+            if (sum < 0)
+                sum = 0;
+            sum += num;
+            if (max < sum)
+                max = sum;
+        }
+        return max;
     }
 };
