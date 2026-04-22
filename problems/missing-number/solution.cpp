99bdd366d212121f536279670b669317e359f894
iijung <987kmj4@naver.com>
2026-04-22 23:16:43 +0900

    solve: missing-number

--- problems/missing-number/solution.cpp
+++ problems/missing-number/solution.cpp
@@ -3,5 +3,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        const int n = nums.size();
+        int sum = n * (n + 1) / 2;
+        for (const int& num: nums) {
+            sum -= num;
+        }
+        return sum;
     }
 };
