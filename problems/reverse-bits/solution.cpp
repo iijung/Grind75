c204457866a041c39eb035b3effb07fd11e9892a
iijung <987kmj4@naver.com>
2026-04-23 00:29:27 +0900

    solve: reverse-bits

--- problems/reverse-bits/solution.cpp
+++ problems/reverse-bits/solution.cpp
@@ -3,5 +3,12 @@
 class Solution {
 public:
     int reverseBits(int n) {
+        int x = 0;
+        for (int i = 0; i < 32; ++i) {
+            x <<= 1;
+            x |= n & 1;
+            n >>= 1;
+        }
+        return x;
     }
 };
