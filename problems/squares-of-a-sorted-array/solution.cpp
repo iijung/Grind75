593e47df37318f096ee8d4c35ceedfd74ff78e98
iijung <987kmj4@naver.com>
2026-04-23 01:11:26 +0900

    solve: squares-of-a-sorted-array

--- problems/squares-of-a-sorted-array/solution.cpp
+++ problems/squares-of-a-sorted-array/solution.cpp
@@ -3,5 +3,19 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int left = 0;
+        int right = nums.size() - 1;
+        int curr = nums.size() - 1;
+        while (left <= right) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                result[curr--] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[curr--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
     }
 };
