9cf50e952c51d7001739e491797721982371f52c
iijung <987kmj4@naver.com>
2026-04-22 22:41:22 +0900

    solve: move-zeros

--- problems/move-zeroes/solution.cpp
+++ problems/move-zeroes/solution.cpp
@@ -3,5 +3,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int curr = 0;
+        int check = 0;
+        while (check < nums.size()) {
+            if (nums[check] != 0) {
+                nums[curr] = nums[check];
+                curr++;
+            }
+            check++;
+        }
+        while (curr < nums.size()) {
+            nums[curr] = 0;
+            curr++;
+        }
     }
 };
