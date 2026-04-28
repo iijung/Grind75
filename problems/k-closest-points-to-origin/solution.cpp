56a9742fb3a5bc2ad1ca6b4c9b6c78dcd732e9b0
iijung <987kmj4@naver.com>
2026-04-28 23:16:47 +0900

    solve: k-closest-points-to-origin

--- problems/k-closest-points-to-origin/solution.cpp
+++ problems/k-closest-points-to-origin/solution.cpp
@@ -3,5 +3,19 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<double, vector<int>>> q;
+        for (auto& point: points) {
+            const double distance = hypot(point[0], point[1]);
+            q.push({distance, point});
+            if (q.size() > k)
+                q.pop();
+        }
+        vector<vector<int>> result;
+        while (!q.empty()) {
+            const auto& [_, point] = q.top();
+            result.push_back(point);
+            q.pop();
+        }
+        return result;
     }
 };
