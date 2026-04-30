257bf9d2df66a827097d20590b9217d4aaf90323
iijung <987kmj4@naver.com>
2026-05-01 04:47:35 +0900

    solve: 3sum

--- problems/3sum/solution.cpp
+++ problems/3sum/solution.cpp
@@ -3,5 +3,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        for (int i = 0; i < nums.size(); i++) {
+            const int target = -nums[i];
+
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                const int sum = nums[j] + nums[k];
+                if (sum < target) {
+                    j++;
+                } else if (sum > target) {
+                    k--;
+                } else if (sum == target) {
+                    const vector<int> triplet = {nums[i], nums[j], nums[k]};
+                    result.push_back(triplet);
+                    while (j < k && nums[j] == triplet[1]) j++;
+                    while (j < k && nums[k] == triplet[2]) k--;
+                }
+            }
+            while (i < nums.size() - 1 && nums[i] == nums[i + 1]) i++;
+        }
+        return result;
     }
 };
