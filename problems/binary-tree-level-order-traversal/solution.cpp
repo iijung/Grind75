baff52431e89463a5cc3ccdc52e66d2396980eef
iijung <987kmj4@naver.com>
2026-05-04 00:26:03 +0900

    solve: binary-tree-level-order-traversal

--- problems/binary-tree-level-order-traversal/solution.cpp
+++ problems/binary-tree-level-order-traversal/solution.cpp
@@ -14,5 +14,25 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (root == nullptr)
+            return result;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            vector<int> level_node;
+            const int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+                level_node.push_back(node->val);
+            }
+            result.push_back(level_node);
+        }
+        return result;
     }
 };
