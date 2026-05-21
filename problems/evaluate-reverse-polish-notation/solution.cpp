0e7f6999160cd7a76c6fa3ca4ccec9761821ec57
iijung <987kmj4@naver.com>
2026-05-22 02:45:03 +0900

    solve: evaluate-reverse-polish-notation

--- problems/evaluate-reverse-polish-notation/solution.cpp
+++ problems/evaluate-reverse-polish-notation/solution.cpp
@@ -2,6 +2,30 @@
 
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
+    int calc(stack<int>& rpn, const char op) {
+        assert(rpn.size() >= 2 && "insufficient operands for operator");
+        const int num2 = rpn.top(); rpn.pop();
+        const int num1 = rpn.top(); rpn.pop();
+        switch (op) {
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            case '*': return num1 * num2;
+            case '/': {
+                assert(num2 != 0 && "division by zero");
+                return num1 / num2;
+            }
+        }
+        return 0;
     }
+
+    int evalRPN(vector<string>& tokens) {
+        stack<int> rpn; // reverse polish notation
+        for (const string& token: tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/")
+                rpn.push(calc(rpn, token[0]));
+            else
+                rpn.push(stoi(token));
+        }
+        assert(rpn.size() == 1 && "invalid reverse polish notation");
+        return rpn.top();
 };
