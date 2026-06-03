11c3f4f7fc8bbd9938efae7e742a1c31b3e9ff05
iijung <987kmj4@naver.com>
2026-06-04 01:35:56 +0900

    solve: produect-of-array-except-self

--- problems/product-of-array-except-self/solution.cpp
+++ problems/product-of-array-except-self/solution.cpp
@@ -4,25 +4,15 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> result(nums.size(), 1);
-        vector<int> prefix(nums.size(), 1);
-        vector<int> suffix(nums.size(), 1);
 
-        prefix[0] = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            prefix[i] = prefix[i - 1] * nums[i];
-        }
-
-        const int end = nums.size() - 1;
-        suffix[end] = nums[end];
-        for (int i = end - 1; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] * nums[i];
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i != 0)
-                result[i] *= prefix[i - 1];
-            if (i != nums.size() - 1)
-                result[i] *= suffix[i + 1];
+        int prefix = 1;
+        int suffix = 1;
+        for (int l = 0; l < nums.size(); l++) {
+            const int r = nums.size() - 1 - l;
+            result[l] *= prefix;
+            prefix *= nums[l];
+            result[r] *= suffix;
+            suffix *= nums[r];
         }
         return result;
     }
