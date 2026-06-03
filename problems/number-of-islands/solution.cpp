737a07522a46a8dc72f9ed6489ebc3d1c70d4d30
iijung <987kmj4@naver.com>
2026-06-04 04:06:09 +0900

    refactor: number-of-islands without visited

--- problems/number-of-islands/solution.cpp
+++ problems/number-of-islands/solution.cpp
@@ -11,40 +11,38 @@ private:
 
 public:
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
-
         int count = 0;
         for (size_t row = 0; row < grid.size(); row++) {
             for (size_t col = 0; col < grid[row].size(); col++) {
-                if (visited[row][col] || grid[row][col] == WATER)
+                if (grid[row][col] == WATER)
                     continue;
                 count++;
-                // dfs(visited, grid, row, col);
-                bfs(visited, grid, row, col);
+                // dfs(grid, row, col);
+                bfs(grid, row, col);
             }
         }
         return count;
     }
 
-    void dfs(vector<vector<bool>>& visited, vector<vector<char>>& grid, int row, int col) {
-        if (!is_range(grid, row, col) || visited[row][col] || grid[row][col] == WATER)
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (!is_range(grid, row, col) || grid[row][col] == WATER)
             return;
-        visited[row][col] = true;
-        dfs(visited, grid, row - 1, col);
-        dfs(visited, grid, row + 1, col);
-        dfs(visited, grid, row, col - 1);
-        dfs(visited, grid, row, col + 1);
+        grid[row][col] = WATER;
+        dfs(grid, row - 1, col);
+        dfs(grid, row + 1, col);
+        dfs(grid, row, col - 1);
+        dfs(grid, row, col + 1);
     }
 
-    void bfs(vector<vector<bool>> visited, vector<vector<char>>& grid, int row, int col) {
+    void bfs(vector<vector<char>>& grid, int row, int col) {
         queue<pair<int, int>> q;
         q.push({row, col});
         while (!q.empty()) {
             const auto [r, c] = q.front();
             q.pop();
-            if (!is_range(grid, r, c) || visited[r][c] || grid[r][c] == WATER)
+            if (!is_range(grid, r, c) || grid[r][c] == WATER)
                 continue;
-            visited[r][c] = true;
+            grid[r][c] = WATER;
             q.push({r - 1, c});
             q.push({r + 1, c});
             q.push({r, c - 1});
