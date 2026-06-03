b3f12c2dbc4ef37ca71adac7e5619183b4036927
iijung <987kmj4@naver.com>
2026-06-04 03:24:03 +0900

    solve: number-of-islands with dfs

--- problems/number-of-islands/solution.cpp
+++ problems/number-of-islands/solution.cpp
@@ -1,7 +1,37 @@
 // https://leetcode.com/problems/number-of-islands/
 
 class Solution {
+private:
+    static constexpr char WATER = '0';
+    static constexpr char LAND = '1';
+
 public:
     int numIslands(vector<vector<char>>& grid) {
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        return dfs(visited, grid, 0, 0);
+    }
+
+    int dfs_recur(vector<vector<bool>>& visited, vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[row].size())
+            return 0;
+        if (visited[row][col] || grid[row][col] == WATER)
+            return 0;
+        visited[row][col] = true;
+        dfs_recur(visited, grid, row - 1, col);
+        dfs_recur(visited, grid, row + 1, col);
+        dfs_recur(visited, grid, row, col - 1);
+        dfs_recur(visited, grid, row, col + 1);
+        return 1;
+    }
+
+    int dfs(vector<vector<bool>> visited, vector<vector<char>>& grid, int row, int col) {
+        int count = 0;
+        for (size_t row = 0; row < grid.size(); row++) {
+            for (size_t col = 0; col < grid[row].size(); col++) {
+                if (!visited[row][col] && grid[row][col] == LAND)
+                    count += dfs_recur(visited, grid, row, col);
+            }
+        }
+        return count;
     }
 };
