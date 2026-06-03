d28faff788601d2972ab3811bec694d7ae6ca121
iijung <987kmj4@naver.com>
2026-06-03 19:26:39 +0900

    solve: implement-trie-prefix-tree (slow)

--- problems/implement-trie-prefix-tree/solution.cpp
+++ problems/implement-trie-prefix-tree/solution.cpp
@@ -2,16 +2,43 @@
 
 class Trie {
 public:
+    bool is_end;
+    unordered_map<char, Trie*> children;
+
     Trie() {
+        is_end = false;
     }
 
     void insert(string word) {
+        if (word.empty())
+            return;
+        Trie* curr = this;
+        for (const char& c: word) {
+            if (curr->children[c] == nullptr)
+                curr->children[c] = new Trie();
+            curr = curr->children[c];
+        }
+        curr->is_end = true;
     }
 
     bool search(string word) {
+        if (word.empty())
+            return is_end;
+        const auto& curr = children.find(word[0]);
+        if (curr == children.end())
+            return false;
+        const auto& [_, next] = *curr;
+        return next->search(word.substr(1));
     }
 
     bool startsWith(string prefix) {
+        if (prefix.empty())
+            return true;
+        const auto& curr = children.find(prefix[0]);
+        if (curr == children.end())
+            return false;
+        const auto& [_, next] = *curr;
+        return next->startsWith(prefix.substr(1));
     }
 };
 
