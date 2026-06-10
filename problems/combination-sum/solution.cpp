6359212c0a219f5668b08e9920159547f6d8d2aa
iijung <987kmj4@naver.com>
2026-06-11 02:58:06 +0900

    solve: combination-sum

--- problems/combination-sum/solution.cpp
+++ problems/combination-sum/solution.cpp
@@ -3,5 +3,18 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+
+        vector<vector<vector<int>>> dp(target + 1);
+        dp[0] = {{}};
+        for (const int x: candidates) {
+            for (int sum = x; sum <= target; sum++) {
+                for (auto sub: dp[sum - x]) {
+                    sub.push_back(x);
+                    dp[sum].push_back(sub);
+                }
+            }
+        }
+        return dp[target];
     }
 };
