739a08eb17ea011e660bd32e54f55b6b0c5360fd
iijung <987kmj4@naver.com>
2026-07-09 12:01:23 +0900

    solve: permutations without std::next_permutation

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -2,6 +2,7 @@
 
 class Solution {
 public:
+#if 0
     vector<vector<int>> permute(vector<int>& nums) {
         sort(nums.begin(), nums.end());
 
@@ -11,4 +12,27 @@ public:
         } while (next_permutation(nums.begin(), nums.end()));
         return result;
     }
+#else
+    vector<vector<int>> permute(vector<int>& nums) {
+        if (nums.empty()) {
+            return {{}};
+        }
+        if (nums.size() == 1) {
+            return {{nums}};
+        }
+        vector<vector<int>> result;
+        for (int i = 0; i < nums.size(); i++) {
+            vector<int> remain;
+            for (const int& x: nums) {
+                if (nums[i] != x)
+                    remain.push_back(x);
+            }
+            for (auto sub: permute(remain)) {
+                sub.push_back(nums[i]);
+                result.push_back(sub);
+            }
+        }
+        return result;
+    }
+#endif
 };
